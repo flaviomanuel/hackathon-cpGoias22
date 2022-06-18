d7385ff0b915d85448bb805e6226c35edf08b710
ryan.fauder <ryan.fauder@gmail.com>
2022-06-18 02:01:49 -0300

    feat(output): implemented merge files

--- output.py
+++ output.py
@@ -1,7 +1,8 @@
 import ffmpeg
-def merge_files(string_video, string_audio, input_transcription, outpu):
+def merge_files(string_video, string_audio):
     stream_video = ffmpeg.input(string_video)
     stream_audio = ffmpeg.input(string_audio)
 
-    merged  = ffmpeg.concat(stream_video, stream_audio, v=1, a=1)
-    output  = ffmpeg.output(merged[0], merged[1], "merged.mp4")
+    merged = ffmpeg.concat(stream_video, stream_audio, v = 1, a = 1)
+    merged.output(merged[0], merged[1], "output.mp4")
+    merged.run()
