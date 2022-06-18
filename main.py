cefb06d229a94c9493d5faf9fe070c14f69bfd07
ryan.fauder <ryan.fauder@gmail.com>
2022-06-18 03:36:48 -0300

    feat(output): merge_files implemented

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import sys
-if(__name__ == "__main__"):
+import output
 
+if(__name__ == "__main__"):
+    input_path = ''
+    output_path = ''
     # -i=file, especifies the input file
     # -o=file, especifies the output file
     opts = [opt for opt in sys.argv[1:] if opt.startswith("-")]
@@ -11,3 +14,5 @@ if(__name__ == "__main__"):
         elif argumento[0:3] == '-o=':
             output_path = argumento[3:]
     print(input_path, output_path)
+    output.merge_files(input_path, "./output/wav/video.wav", output_path)
+    # python main.py -i="./audios/video.mp4" -o="./output/mp4/output.mp4"
--- output.py
+++ output.py
@@ -1,8 +1,15 @@
-import ffmpeg
-def merge_files(string_video, string_audio):
-    stream_video = ffmpeg.input(string_video)
-    stream_audio = ffmpeg.input(string_audio)
+# import ffmpeg
+import moviepy.editor as mp
+def merge_files(input_string_video, string_audio, output_string_video):
+    audio = mp.AudioFileClip(string_audio)
+    video = mp.VideoFileClip(input_string_video)
+    
+    final_video = video.set_audio(audio)
 
-    merged = ffmpeg.concat(stream_video, stream_audio, v = 1, a = 1)
-    merged.output(merged[0], merged[1], "output.mp4")
-    merged.run()
+    final_video.write_videofile(output_string_video)
+
+# def merge_files(input_string_video, string_audio, output_string_video):
+#     stream_video = ffmpeg.input(input_string_video)
+#     stream_audio = ffmpeg.input(string_audio)
+
+#     merged = ffmpeg.concat(stream_video, stream_audio, v = 1, a = 1).output(output_string_video).run()
