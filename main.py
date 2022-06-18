cbcc76b656763c04af80ded8516b001f72fb4dde
vasconcelos-Marcos <vasconcelmarcos@gmail.com>
2022-06-18 04:26:00 -0300

    catch extension

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import sys
-import output
 
 if(__name__ == "__main__"):
     input_path = ''
@@ -7,12 +6,12 @@ if(__name__ == "__main__"):
     # -i=file, especifies the input file
     # -o=file, especifies the output file
     opts = [opt for opt in sys.argv[1:] if opt.startswith("-")]
-    for argum in opts:
-        argumento = argum
+    for argumento in opts:
         if argumento[0:3] == '-i=':
             input_path = argumento[3:]
+            input_extension = argumento[(argumento.rfind('.')+1):]
         elif argumento[0:3] == '-o=':
             output_path = argumento[3:]
-    print(input_path, output_path)
-    output.merge_files(input_path, "./output/wav/video.wav", output_path)
+    print(input_path, output_path, input_extension)
+    #output.merge_files(input_path, "./output/wav/video.wav", output_path)
     # python main.py -i="./audios/video.mp4" -o="./output/mp4/output.mp4"
