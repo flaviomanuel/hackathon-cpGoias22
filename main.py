3f9d899f1abe0fd70b5862bd4a0a520202ecc85f
vasconcelos-Marcos <vasconcelmarcos@gmail.com>
2022-06-18 01:59:22 -0300

    CLI

--- main.py
+++ main.py
@@ -4,7 +4,10 @@ if(__name__ == "__main__"):
     # -i=file, especifies the input file
     # -o=file, especifies the output file
     opts = [opt for opt in sys.argv[1:] if opt.startswith("-")]
-
-    args = [args for args in sys.argv[1:] if not args.startswith("-")]
-    print(opts)
-
+    for argum in opts:
+        argumento = argum
+        if argumento[0:3] == '-i=':
+            input_path = argumento[3:]
+        elif argumento[0:3] == '-o=':
+            output_path = argumento[3:]
+    print(input_path, output_path)
